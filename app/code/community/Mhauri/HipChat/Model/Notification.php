701ee2e0866a915a84a42f890c85d1b9e8809810
Marcel Hauri <marcel@hauri.me>
2015-04-27 21:19:09 +0200

    add first notifications

--- app/code/community/Mhauri/HipChat/Model/Notification.php
+++ app/code/community/Mhauri/HipChat/Model/Notification.php
@@ -22,32 +22,131 @@
  * @author Marcel Hauri <marcel@hauri.me>
  */
 
-include_once 'HipChat/HipChat.php';
+require_once(Mage::getBaseDir('lib') . '/Atlassian/HipChat/HipChat.php');
 
 class Mhauri_HipChat_Model_Notification extends Mage_Core_Model_Abstract
 {
 
-    const LOG_FILE          = 'hipchat.log';
+    const LOG_FILE                      = 'hipchat.log';
+    const DEFAULT_SENDER                = 'Magento HipChat';
 
-    const ENABLE_PATH       = 'hipchat/general/enable';
-    const LOG_PATH          = 'hipchat/general/log';
-    const TOKEN_PATH        = 'hipchat/general/token';
-    const ROOM_ID_PATH      = 'hipchat/general/room_id';
+    const ENABLE_PATH                   = 'hipchat/general/enable';
+    const LOG_PATH                      = 'hipchat/general/log';
+    const TOKEN_PATH                    = 'hipchat/general/token';
+    const ROOM_ID_PATH                  = 'hipchat/general/room_id';
+    const FROM_NAME_PATH                = 'hipchat/general/from_name';
 
-    const NEW_ORDER_PATH    = 'hipchat/notification/new_order';
+    const NEW_ORDER_PATH                = 'hipchat/notification/new_order';
+    const ADMIN_USER_LOGIN_FAILED_PATH  = 'hipchat/notification/admin_user_login_failed';
 
-    private $_hipchat   = null;
+    /**
+     * Store the HipChat Model
+     * @var null
+     */
+    private $_hipchat       = null;
 
-    private $_message   = '';
+    /**
+     * Store the Message
+     * @var string
+     */
+    private $_message       = '';
 
-    private $_from      = 'TEST';
+    /**
+     * Store the from name
+     * @var string
+     */
+    private $_fromName      = null;
+
+    /**
+     * Store room id
+     * @var null
+     */
+    private $_roomId        = null;
+
+    /**
+     * Store token
+     * @var null
+     */
+    private $_token         = null;
+
 
     public function _construct()
     {
-        parent::_construct();
+        $this->setToken(Mage::getStoreConfig(self::TOKEN_PATH, 0));
+        $this->setFromName(Mage::getStoreConfig(self::FROM_NAME_PATH, 0));
+        $this->setRoomId(Mage::getStoreConfig(self::ROOM_ID_PATH, 0));
+
         if($this->isEnabled()) {
-            $this->_hipchat = new HipChat(Mage::getStoreConfig(self::TOKEN_PATH, 0));
+            $this->_hipchat = new HipChat\HipChat($this->getToken());
         }
+        parent::_construct();
+    }
+
+    /**
+     * @param string $token
+     * @return $this
+     */
+    public function setToken($token)
+    {
+        if(is_string($token)) {
+            $this->_token = $token;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getToken()
+    {
+        return $this->_token;
+    }
+
+    /**
+     * @param $name
+     * @return $this
+     */
+    public function setFromName($name)
+    {
+        if(is_string($name)) {
+            $this->_fromName = $name;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFromName()
+    {
+        if($this->_fromName) {
+            return $this->_fromName;
+        }
+
+        return '';
+    }
+
+    /**
+     * @param $id
+     * @return $this
+     */
+    public function setRoomId($id)
+    {
+        if(is_numeric($id)) {
+            $this->_roomId = $id;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return null
+     */
+    public function getRoomId()
+    {
+        return $this->_roomId;
     }
 
     /**
@@ -71,18 +170,6 @@ class Mhauri_HipChat_Model_Notification extends Mage_Core_Model_Abstract
     }
 
     /**
-     * send message
-     */
-    public function send()
-    {
-        if(Mage::getStoreConfig(self::LOG_PATH, 0)) {
-            Mage::log($this->_message, Zend_Log::INFO, self::LOG_FILE, true);
-        }
-
-        $this->_hipchat->message_room($this->getRoomId(), $this->_from, $this->getMessage());
-    }
-
-    /**
      * @return mixed
      */
     public function isEnabled()
@@ -91,10 +178,24 @@ class Mhauri_HipChat_Model_Notification extends Mage_Core_Model_Abstract
     }
 
     /**
-     * @return mixed
+     * send message to room
      */
-    public function getRoomId()
+    public function send()
     {
-        return Mage::getStoreConfig(self::ROOM_ID_PATH, 0);
+        try {
+            $this->_hipchat->message_room($this->getRoomId(), $this->getFromName(), $this->getMessage());
+            if(Mage::getStoreConfig(self::LOG_PATH, 0)) {
+                Mage::log('Message sent: ' . $this->_message, Zend_Log::INFO, self::LOG_FILE, true);
+            }
+        } catch(Exception $e) {
+            $params = array(
+                'from:'     => $this->getFromName(),
+                'message:'  => $this->getMessage(),
+                'token:'    => $this->getToken()
+            );
+
+            Mage::log($params, Zend_Log::ERR, self::LOG_FILE, true);
+            Mage::log($e->getMessage(), Zend_Log::ERR, self::LOG_FILE, true);
+        }
     }
 }
--- app/code/community/Mhauri/HipChat/Model/Observer.php
+++ app/code/community/Mhauri/HipChat/Model/Observer.php
@@ -1,23 +1,47 @@
 <?php
 
-class Mhauri_HipChat_Model_Notification
+class Mhauri_HipChat_Model_Observer
 {
     private $_notificationModel = null;
+
+    private $_helper = null;
 
     public function __construct()
     {
         $this->_notificationModel = Mage::getSingleton('mhauri_hipchat/notification');
+        $this->_helper = Mage::helper('mhauri_hipchat');
     }
 
+    /**
+     * Send a notification when a new order was placed
+     * @param $observer
+     */
     public function notifyNewOrder($observer)
     {
-        $test = $observer;
-        $this->_notificationModel->setMessage('test')->send();
+        if($this->_getConfig(Mhauri_HipChat_Model_Notification::NEW_ORDER_PATH)) {
+            $message = $this->_helper->__('<strong>A new order has been placed.</strong><br /><strong>Order Id:</strong> %s<br /><strong>Name:</strong> %s %s<br /><strong>Amount:</strong> %s',
+                $observer->getOrder()->getIncrementId(),
+                $observer->getOrder()->getCustomer()->getFirstname(),
+                $observer->getOrder()->getCustomer()->getLastname(),
+                $observer->getOrder()->getQuoteBaseGrandTotal());
+
+            $this->_notificationModel->setMessage($message)->send();
+        }
     }
 
-    public function notifyUserLogin($observer)
+    /**
+     * Send a notification when admin user login failed
+     * @param $observer
+     */
+    public function notifiyAdminUserLoginFailed($observer)
     {
-        $test = $observer;
-        $this->_notificationModel->setMessage('test')->send();
+        if($this->_getConfig(Mhauri_HipChat_Model_Notification::ADMIN_USER_LOGIN_FAILED_PATH)) {
+            $this->_notificationModel->setMessage($this->_helper->__('Admin user login failed with username: %s', $observer->getUserName()))->send();
+        }
+    }
+
+    private function _getConfig($path)
+    {
+        return Mage::getStoreConfig($path, 0);
     }
 }
